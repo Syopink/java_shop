1314819d56173a61bf6e7fd9caaa60700795f891
nguyenanninh1212212 <ninh56085@gmail.com>
2024-11-29 23:05:29 +0700

    new

--- src/main/java/com/mycompany/vietpro/Homepage.java
+++ src/main/java/com/mycompany/vietpro/Homepage.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */ 
+ */
 package com.mycompany.vietpro;
 
 import Interface.login;
@@ -25,13 +25,14 @@ public final class Homepage extends javax.swing.JFrame {
 
     private static user us;
     /**
+     * Creates new form Homepage
      */
-<<<<<<< HEAD
     private int posX = 0, posY = 0;
 
     public Homepage(user us) {
         this.us = us;
-        setUndecorated(true);
+       if(us !=null){
+            setUndecorated(true);
         initComponents();
         setResizable(false);
         addMouseListener(new MouseAdapter() {
@@ -42,22 +43,6 @@ public final class Homepage extends javax.swing.JFrame {
                 posY = e.getY();
             }
         });
-=======
-      private int posX = 0, posY = 0;   
-       public Homepage(user us) {
-           this.us = us;
-           System.out.println("com.mycompany.vietpro.Homepage.<init>()"+ us.getEmail());
-            setUndecorated(true);
-            initComponents();
-            setResizable(false);
-            addMouseListener(new MouseAdapter() {
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    posX = e.getX();
-                    posY = e.getY();
-                }
-            });
->>>>>>> b67c72cc9629bb942ca50c74ec2cf6b0746cf851
 
         addMouseMotionListener(new MouseAdapter() {
             @Override
@@ -67,14 +52,8 @@ public final class Homepage extends javax.swing.JFrame {
             }
         });
         this.setLocationRelativeTo(null);
-        setUpPanels();
+        setUpPanels();}
     }
-<<<<<<< HEAD
-=======
-            public static user getUser(){
-                return Homepage.us;
-            }
->>>>>>> b67c72cc9629bb942ca50c74ec2cf6b0746cf851
 
     public static user getUser() {
         return Homepage.us;
@@ -186,18 +165,8 @@ public final class Homepage extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-<<<<<<< HEAD
             Homepage homepage = new Homepage(us);
             homepage.setVisible(true);
-=======
-            Homepage homepage;
-                 if(us != null){
-                    homepage=new Homepage(us);
-                    homepage.setVisible(true);
-                 }else{
-                     new login().setVisible(true);
-                 }
->>>>>>> b67c72cc9629bb942ca50c74ec2cf6b0746cf851
         });
     }
 
